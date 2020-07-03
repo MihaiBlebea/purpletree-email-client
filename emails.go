8db95191f0c648962234fc9e6840c46ca72c2db1
Mihai Blebea <mihai@getchip.uk>
2020-07-03 19:39:22 +0100

    Adds more emails and subject templates

--- emails.go
+++ emails.go
@@ -5,8 +5,10 @@ const (
 	WelcomeEmail    string = "welcome"
 	ConfirmEmail    string = "confirm-email"
 	ConfirmPassword string = "confirm-password"
-	ContactEmail    string = "contact"
-	ContactPayment  string = "payment"
+	Contact         string = "contact"
+	AdminContact    string = "admin-contact"
+	Payment         string = "payment"
+	AdminPayment    string = "admin-payment"
 )
 
 // Subject template constants
@@ -14,6 +16,8 @@ const (
 	WelcomeSubject         string = "Welcome %s, download link inside"
 	ConfirmEmailSubject    string = "One last step, confirm your email %s"
 	ConfirmPasswordSubject string = "Reset password, confirm your email"
-	ContactEmailSubject    string = "Somebody is looking for you"
-	ContactPaymentSubject  string = "Thank you %s, here are your details"
+	ContactSubject         string = "We received your message"
+	ContactAdminSubject    string = "Somebody is looking for you"
+	PaymentSubject         string = "Thank you %s, here are your details"
+	AdminPaymentSubject    string = "Somebody bought a product"
 )
